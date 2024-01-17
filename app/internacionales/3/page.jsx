7189e96d17ebdf34e5f9952ce0b9afbfcdd17123
Joel Ramos <joelramos456@gmail.com>
2024-01-17 15:42:16 -0500

    inter

--- app/internacionales/3/page.jsx
+++ app/internacionales/3/page.jsx
@@ -5,13 +5,13 @@ export default function Page() {
       <h1 className="fs-1" style={{ color: '#d1510f' }}>RUSIA REALIZA UN NUEVO ATAQUE A UCRANIA</h1>
       <div className="fs-5">
         <p>
-          Un total de aproximadamente 110 misiles fueron lanzados contra Ucrania, la mayoría de los cuales fueron derribados, escribió Volodímir Zelenski (insertar hipervínculo: https://larepublica.pe/mundo/2023/12/29/rusia-lanza-nuevo-ataque-masivo-contra-ucrania-deja-al-menos-12-muertos-y-decenas-de-heridos-video-misiles-1211765) , el presidente ucraniano, a través de X (antes Twitter).
+          Un total de aproximadamente 110 misiles fueron lanzados contra Ucrania, la mayoría de los cuales fueron derribados, escribió  <a href="https://larepublica.pe/mundo/2023/12/29/rusia-lanza-nuevo-ataque-masivo-contra-ucrania-deja-al-menos-12-muertos-y-decenas-de-heridos-video-misiles-1211765">Volodímir Zelenski</a> , el presidente ucraniano, a través de X (antes Twitter).
         </p>
         <div className="d-flex justify-content-center my-5">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/HQCtauuPj88?si=KCrydMtQCj7wkFH_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         <p>
-          El presidente Volodímir Zelenski afirmó hoy, viernes 29 de diciembre de 2023, que, como parte de los masivos ataques rusos, las fuerzas de Moscú lanzaron unos 110 misiles contra territorio de Ucrania (insertar hipervínculo: https://larepublica.pe/mundo/2023/12/15/concejal-detona-varias-granadas-en-plena-reunion-y-muere-en-el-acto-explosion-deja-26-heridos-serhii-batryn-ucrania-1232550) , pero aseguró que la mayoría de ellos ha sido derribada, aunque han muerto al menos 12 personas.
+          El presidente Volodímir Zelenski afirmó hoy, viernes 29 de diciembre de 2023, que, como parte de los masivos ataques rusos, las fuerzas de Moscú lanzaron unos 110 misiles contra territorio de  <a href="https://larepublica.pe/mundo/2023/12/15/concejal-detona-varias-granadas-en-plena-reunion-y-muere-en-el-acto-explosion-deja-26-heridos-serhii-batryn-ucrania-1232550">Ucrania</a> , pero aseguró que la mayoría de ellos ha sido derribada, aunque han muerto al menos 12 personas.
         </p>
         <p>
           Hoy Rusia ha usado casi todos los tipos de armas de su arsenal: (misiles) Kinzhal, S-300, misiles de crucero y drones. Los bombarderos estratégicos lanzaron misiles X-101 y X-505. Un total de aproximadamente 110 misiles fueron lanzados contra Ucrania, la mayoría de los cuales fueron derribados, escribió a través de X (antes Twitter).
